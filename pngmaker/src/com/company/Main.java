20422f8d1073107fc6efff96df186359fc1d1f8e
Justin599M <98783240+Justin599M@users.noreply.github.com>
2022-01-31 14:45:38 -0800

    Add files via upload

---
+++ pngmaker/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import com.pngmaker.maker;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// 2% for white ,44% for grey, 44% for darksteel
+        maker maker = new maker();
+        maker.imagemaker(562,1000);
+        System.out.println("done");
+    }
+}
---
+++ pngmaker/src/com/pngmaker/Main.java
@@ -0,0 +1,10 @@
+package com.pngmaker;
+
+public class Main {
+
+    public static void main(String[] args) {
+	maker maker = new maker();
+    maker.imagemaker(400,711);
+    System.out.println("done1");
+    }
+}
